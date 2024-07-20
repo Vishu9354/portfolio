d2c82bf0caea86a67aafaf4680427a8968ac64aa
Vishu9354 <vishu0883386@gmail.com>
2024-07-20 22:03:58 +0530

    update

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -24,12 +24,72 @@ export default function Portfolio() {
         </div>
         <div className='right'>
             <div className='intro font'>
-                I am Vishal kumar currently Studying at IIT Jammu. Also passinated in development. With Strong knowlegde of HTML, CSS, Javascript a nd React Js. Also Work with Different College Teams and built user-friendly Website for college fest. Also Developed Website for College club Nature and Adventure Club IIT Jammu. As I Already worked with different teams in my college. I am good in managing and working different teams. Also have experinced in working alone. I am good at compliting my projects before deadline. While working on different projects my main focus is to build user friendly interface and optimizing preformance to ensure fast load time this helps to enhance users experience on website.<br/><br/>
-                Explore my projects and if you are looking for frontend developer connect with me and discuss how I can contribute to your project
+            I am Vishal kumar currently Studying at IIT Jammu. Also passionate about development. With Strong knowledge of HTML, CSS, Javascript and React JS. I also worked with different college teams and built a user-friendly website for the college fest. I also developed a Website for the College Club Nature and Adventure Club IIT Jammu. As I already worked with different teams in my college. I am good at managing and working with different teams. Also, have experience in working alone. I am good at completing my projects before the deadline. While working on different projects my main focus is to build a user-friendly interface and optimise performance to ensure fast load time this helps to enhance users' experience on the website.<br/><br/>
+            Explore my projects and if you are looking for a frontend developer connect with me and discuss how I can contribute to your project.
 
             </div>
 
-            <div className='project font'>
+            <div className='project'>
+                <a href='#'>
+                <div className='projectdetail font'>
+                    <div className='time'>
+                    2024
+                    </div>
+                    <div className='prjright'>
+                        <div className='prjname'>
+                          <b>  Web Devloper Nature and Adventure Club IIT Jammu</b>
+                        </div>
+                        <div className='prjdetail'>
+                            Develop Website for Nature and Adventure Club IIT Jammu.Ensured a user-friendly and responsive website design by utilizing React Js. Implemented various features,club
+                            information, and member profiles.
+                        </div>
+                        <div className='skills'>
+                            <span className='skill'>
+                                HTML
+                            </span>
+                            <span className='skill'>
+                              CSS
+                            </span>
+                            <span className='skill'>
+                                Javascript
+                            </span>
+                            <span className='skill'>
+                                ReactJs
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                </a>
+                <a href='#'>
+                <div className='projectdetail font'>
+                    <div className='time'>
+                    2024
+                    </div>
+                    <div className='prjright'>
+                        <div className='prjname'>
+                          <b>  Web Devloper Nature and Adventure Club IIT Jammu</b>
+                        </div>
+                        <div className='prjdetail'>
+                            Develop Website for Nature and Adventure Club IIT Jammu.Ensured a user-friendly and responsive website design by utilizing React Js. Implemented various features,club
+                            information, and member profiles.
+                        </div>
+                        <div className='skills'>
+                            <span className='skill'>
+                                HTML
+                            </span>
+                            <span className='skill'>
+                              CSS
+                            </span>
+                            <span className='skill'>
+                                Javascript
+                            </span>
+                            <span className='skill'>
+                                ReactJs
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                </a>
                 <a href='#'>
                 <div className='projectdetail font'>
                     <div className='time'>
