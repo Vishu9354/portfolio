91df521fc01abdcfd29a5509191d622c44b9a4f6
Vishu9354 <vishu0883386@gmail.com>
2024-07-20 19:25:12 +0530

    intiallize

---
+++ src/Portfolio.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './portfolio.css'
+export default function Portfolio() {
+  return (
+    <>
+        <div className='left'>
+            <div className='name font'>
+                Vishal Kumar
+            </div>
+            <div className='position font'>
+                Developer
+            </div>
+            <div className='detail font'>
+            Building responsive and dynamic web experiences <br/> with a passion for clean code and design
+            </div>
+
+            <div className='navigation font'>
+                <div>
+                <div className='hr'>About</div>
+                <div className='hr'>Skills</div>
+                <div className='hr'>Projects</div>
+                </div>
+            </div>
+        </div>
+        <div className='right'>
+            
+        </div>
+    </>
+  )
+}
